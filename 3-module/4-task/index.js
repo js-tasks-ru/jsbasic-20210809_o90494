9aa549d4e12d5ecdce181284ad54fab9c05a765e
Oksana9494 <56865580+Oksana9494@users.noreply.github.com>
2021-08-30 16:54:55 +0300

    solving of 3 modul's tasks

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,12 @@
 function showSalary(users, age) {
-  // ваш код...
+  let str = '';
+  let arrFiltered = users.filter((item) =>  item['age'] < age || item['age'] == age );
+  arrFiltered.forEach((el, i) => {
+    if((i + 1) == arrFiltered.length) {
+      str += `${el.name}, ${el.balance}`
+    } else {
+      str +=`${el.name}, ${el.balance}\n`
+    }
+  });
+  return str; 
 }
