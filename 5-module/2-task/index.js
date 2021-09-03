65d540dfae5fdf7e719c6debd9f42438470eb2b5
Oksana9494 <56865580+Oksana9494@users.noreply.github.com>
2021-09-03 20:34:43 +0300

    solving of 5 modul's tasks

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,9 @@
 function toggleText() {
-  // ваш код...
+  let btns = document.getElementsByClassName('toggle-text-button');
+  let text = document.getElementById('text');
+  for(let btn of btns) {
+    btn.addEventListener('click', event => {
+      text.toggleAttribute('hidden'); 
+    })
+  }
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,37 @@
 function initCarousel() {
-  // ваш код...
+  let btnRight = document.querySelector('.carousel__arrow_right');
+  let btnLeft = document.querySelector('.carousel__arrow_left');
+  let carouselInner = document.querySelector('.carousel__inner');
+  let slides = document.querySelectorAll('.carousel__slide');
+  let width = 0;
+  let currentIndex = 0;
+
+  btnLeft.style.display = 'none';
+
+  let carouselRight = function() {
+    currentIndex++;
+    if(currentIndex == (slides.length - 1)) {
+      btnRight.style.display = 'none';
+    } 
+    if(currentIndex > 0) {
+      btnLeft.style.display = '';
+    }
+    width += slides[currentIndex].offsetWidth;
+    carouselInner.style.transform = 'translateX(-' + width + 'px)';
+  }
+
+  let carouselLeft = function() {
+    currentIndex--;
+    if(currentIndex < (slides.length - 1)) {
+      btnRight.style.display = '';
+    }
+    if(currentIndex == 0) {
+      btnLeft.style.display = 'none';
+    }
+    width -= slides[currentIndex].offsetWidth;
+    carouselInner.style.transform = 'translateX(-' + width + 'px)';
+  }
+
+  btnRight.addEventListener('click', carouselRight);  
+  btnLeft.addEventListener('click', carouselLeft); 
 }
