3b9c082fcfc88eb69617cbfc33a2b7c933e3d484
Oksana Chikunova <o90494@yandex.ru>
2021-09-18 18:33:16 +0300

    solving of 6 modul's tasks

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,39 @@
  *      }
  *
  */
-export default class UserTable {
+ export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    let table = document.createElement('TABLE');
+    this._elem = table;
+    let thead = document.createElement('THEAD');
+    let tbody = document.createElement('TBODY');
+    this._elem.append(thead);
+    this._elem.append(tbody);
+
+    thead.innerHTML = `<tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+      </tr>`;
+     for(let i = 0; i < this.rows.length; i++) {
+      let tr = document.createElement('TR');
+      tbody.append(tr);
+      tr.innerHTML = `
+        <td>${this.rows[i].name}</td>
+        <td>${this.rows[i].age}</td>
+        <td>${this.rows[i].salary}</td>
+        <td>${this.rows[i].city}</td>
+        <td><button>X</button></td>
+      `; 
+      let btn = tr.querySelector('BUTTON');
+      btn.addEventListener('click', event => {
+        event.target.closest('tr').remove();
+      });        
+    }
+  }
+  get elem() {
+    return this._elem;
   }
 }
